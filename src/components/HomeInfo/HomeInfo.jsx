51ea8889448c4dc73500ac5a8c1e34c9e3c59713
martinvruiz <martinvruiz10@gmail.com>
2025-01-02 13:15:04 -0300

    added icon

--- src/components/HomeInfo/HomeInfo.jsx
+++ src/components/HomeInfo/HomeInfo.jsx
@@ -1,48 +1,46 @@
-import { InfoItem } from "../InfoItem/InfoItem"
-import images from "../../icons/icons"
+import { InfoItem } from "../InfoItem/InfoItem";
+import images from "../../icons/icons";
 
-
-
-export const HomeInfo = ()=>{
-
-
-    return <>
-    
-        <div className="w-3/5 flex flex-col items-center text-center my-4">
-            <h3 className="text-3xl my-2 font-bold border-b border-orange-500 w-full p-1">Beneficios de ir al gimnasio</h3>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 items-center justify-center">
-                <InfoItem 
-                    title="Fortalecimiento muscular" 
-                    text="Incrementa la fuerza y tonifica los músculos."
-                    img={images[0]}
-                />
-                <InfoItem 
-                    title="Mejora la resistencia" 
-                    text="Incrementa la capacidad aeróbica y anaeróbica."
-                    img={images[1]}
-                />
-                <InfoItem 
-                    title="Control del peso" 
-                    text="Ayuda a quemar calorías y controlar el peso corporal."
-                    img={images[2]}
-                />
-                <InfoItem 
-                    title="Reducción del estrés" 
-                    text="Libera endorfinas, que mejoran el estado de ánimo y reducen el estrés."
-                    img={images[3]}
-                />
-                <InfoItem 
-                    title="Mayor autoestima" 
-                    text="Alcanzar metas físicas mejora la confianza en uno mismo."
-                    img={images[4]}
-                />
-                <InfoItem 
-                    title="Aumento de la energía diaria" 
-                    text="Mejora la capacidad para realizar tareas cotidianas."
-                    img={images[5]}
-                />
-                </div>
+export const HomeInfo = () => {
+  return (
+    <>
+      <div className="w-3/5 flex flex-col items-center text-center my-4">
+        <h3 className="text-3xl my-2 font-bold border-b border-orange-500 w-full p-1">
+          Beneficios de ir al gimnasio
+        </h3>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 items-center justify-center">
+          <InfoItem
+            title="Fortalecimiento muscular"
+            text="Incrementa la fuerza y tonifica los músculos."
+            img={images[0]}
+          />
+          <InfoItem
+            title="Mejora la resistencia"
+            text="Incrementa la capacidad aeróbica y anaeróbica."
+            img={images[1]}
+          />
+          <InfoItem
+            title="Control del peso"
+            text="Ayuda a quemar calorías y controlar el peso corporal."
+            img={images[2]}
+          />
+          <InfoItem
+            title="Reducción del estrés"
+            text="Libera endorfinas, que mejoran el estado de ánimo y reducen el estrés."
+            img={images[3]}
+          />
+          <InfoItem
+            title="Mayor autoestima"
+            text="Alcanzar metas físicas mejora la confianza en uno mismo."
+            img={images[4]}
+          />
+          <InfoItem
+            title="Aumento de la energía diaria"
+            text="Mejora la capacidad para realizar tareas cotidianas."
+            img={images[5]}
+          />
         </div>
-    
+      </div>
     </>
-}
+  );
+};
